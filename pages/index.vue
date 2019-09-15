c05ada9410142d08568bf7ffdc1f0d2ee5a9f53f
Lee YongJin <jjeaby@gmail.com>
2019-09-16 01:17:10 +0900

    webview add

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,33 @@
 <!--			<a href="https://electronjs.org/" target="_blank" class="btn btn-secondary">Electron</a>-->
 <!--			<a href="https://github.com/electron-userland/electron-builder" target="_blank" class="btn btn-secondary">Electron Builder</a>-->
 <!--		</div>-->
-		<web-view
-				:source="{uri: 'https://github.com/facebook/react-native'}"
-				:style="{marginTop: 20}">
+		<webview
+				id="mstodo"
+				src="https://todo.microsoft.com/login?redirectUrl=%2Fko-kr"
 
-		</web-view>
+		>
+		</webview>
+
 	</section>
 </template>
 
 <script>
 export default {
+	mounted() {
+		const webview = document.querySelector('webview')
+		webview.addEventListener("dom-ready", function() {
+
+
+			if( !webview.src.startsWith("https://login.live.com") &&
+				!webview.src.startsWith("https://todo.microsoft.com") &&
+				!webview.src.startsWith("https://to-do.microsoft.com") &&
+				!webview.src.startsWith("https://account.microsoft.com")  )
+			{
+				webview.src = "https://todo.microsoft.com/login?redirectUrl=%2Fko-kr"
+			}
+			console.log(webview.src);
+
+		});	}
 }
 </script>
 
@@ -30,7 +47,23 @@ export default {
 	align-items: center;
 	text-align: center;
 }
-.btn {
-	margin: 0 8px;
+html, body {
+	width: 100%;
+	height: 100%;
+	margin: 0;
+	padding: 0;
 }
+
+body {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+webview{
+	width:100vw;
+	height:100vh;
+}
+
+
 </style>
